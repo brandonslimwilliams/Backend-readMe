323ebf2efb5e75d5fcc4d77b7c871646dbf1dd75
brandonslimwilliams <brandonslimwilliams@gmail.com>
2022-08-23 17:23:21 -0500

    made some small upgrades

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const questions = () => {
     {
       type: "input",
       name: "title",
-      messege: "What is the name of your repository (Required)",
+      message: "What is the name of your repository (Required)",
       validate: (titleInput) => {
         if (titleInput) {
           return true;
@@ -22,12 +22,12 @@ const questions = () => {
     {
       type: "input",
       name: "description",
-      messege: "What is your repository about (Required)",
+      message: "What is your repository about (Required)",
     },
     {
       type: "input",
       name: "installation",
-      messege: "what dependencies are being used?",
+      message: "what dependencies are being used?",
       validate: (installation) => {
         if (installation) {
           return true;
@@ -39,33 +39,33 @@ const questions = () => {
     {
       type: "input",
       name: "usage",
-      messege: "what is needed for the user to use the project?",
+      message: "what is needed for the user to use the project?",
     },
     {
       type: "input",
       name: "contribution",
-      messege: "what contributions were made to the project?",
+      message: "what contributions were made to the project?",
     },
     {
       type: "input",
       name: "instructions",
-      messege: "how to test project?",
+      message: "how to test project?",
     },
     {
       type: "checkbox",
       name: "license",
-      messege: "please choose a license",
+      message: "please choose a license",
       choices: ["MIT", "IPL_1.0", "NASA_1.3", "NPOSL_3.0", "OSL_3.0", "None"],
     },
     {
       type: "input",
       name: "username",
-      messege: "enter username",
+      message: "enter username",
     },
     {
         type: 'input',
         name: 'email',
-        messege: 'enter email',
+        message: 'enter email',
     },
   ]).then((answers) => {
     fs.writeFile("README.md", generateMarkdown(answers), err => {
